ccd96919cd4cc636cd0a5304030cf26726856999
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-28 00:14:54 +0530

    [Rupali]Feat:Take input from user

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewAddressBook
+{
+    class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailId { get; set; }
+    }
+}
