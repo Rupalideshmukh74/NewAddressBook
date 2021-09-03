0dc4c824bb760dc9b087f7e9a08b739478ba3277
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-09-03 12:48:05 +0530

    [Rupali]Feat:Read write using file io

---
+++ ContactFileOperation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace NewAddressBook
+{
+    class ContactFileOperation
+    {
+        public static void FileOps()
+        {
+            string path = @"C:\Users\DELL\source\repos\NewAddressBook\NewAddressBook\ContactFile.txt";
+            WriteDataToFile(path);
+            ReadDataFromFile(path);
+        }
+
+        static void WriteDataToFile(string path)
+        {
+            Console.WriteLine("\n - - - Writing contacts to file - - - ");
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File exists.");
+                StreamWriter writer = new StreamWriter(path);
+
+                writer.WriteLine("FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,EmailId");
+                foreach (var ab in AddressBookMain.contactsDictionary)
+                {
+                    writer.WriteLine("Inside Address Book : " + ab.Key);
+                    foreach (Person person in AddressBookMain.contactsDictionary[ab.Key])
+                    {
+                        string data =
+                            person.FirstName + "," + person.LastName + "," + person.Address + "," + person.City + "," + person.State + "," + person.ZipCode + "," + person.PhoneNumber + "," + person.EmailId;
+
+                        writer.WriteLine(data);
+                    }
+                }
+                Console.WriteLine(" File Write success.");
+                writer.Close();
+            }
+            else
+            {
+                Console.WriteLine(" Process Not completed.");
+            }
+        }
+
+        static void ReadDataFromFile(string path)
+        {
+            Console.WriteLine("\n - - - Reading contacts from text file - - - ");
+            StreamReader file = new StreamReader(path);
+            using (file)
+            {
+                string data;
+                while ((data = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
+    
--- ContactManager.cs
+++ ContactManager.cs
@@ -4,20 +4,24 @@ using System.Text;
 
 namespace NewAddressBook
 {
-    
+
     class ContactManager
     {
         //options to select operation
         public static void Operations()
         {
-            Console.WriteLine("\n Available options :\n 1.Add_contact\t 2.Edit_contact\t 3.Delete_Contact\t 4.View_contacts\t 5.New_address_book\tSearch_person\t7.DisplayByCityState\t8.GetCount_Ofperson_byCityOrState 0.Exit \n");
-            Console.Write(" Provide option :  ");
+            Console.WriteLine("\n Available options :\n 1.Add_contact\t 2.Edit_contact\t 3.Delete_Contact\t 4.View_contacts\t 5.New_address_book\t 6.Search_person_by_cityOrState\t 7.ViewPerson_ByCityOrState \t 8.Sort_addressBook_contacts \n 9.Read_Write_Contacts_UsingFileOperations \t\t 0.Exit \n");
+            Console.Write("Provide option :  ");
+
+            //userInput
             int userAction = int.Parse(Console.ReadLine());
             string findName, searchAdrBookName;
             switch (userAction)
             {
                 case 1:
-                    Console.Write("\n Enter addressbook name to select and add contact : ");
+
+                    AddressBookMain.DisplayABList();
+                    Console.Write("\nEnter addressbook name to select and add contact : ");
                     searchAdrBookName = Console.ReadLine();
                     CheckAddresssBook(searchAdrBookName);
                     AddressBookMain.AddPersonInfo(searchAdrBookName);
@@ -25,18 +29,16 @@ namespace NewAddressBook
                     break;
 
                 case 2:
+                    AddressBookMain.DisplayABList();
                     Console.Write("\n  Enter addressbook name to find and edit contact : ");
                     searchAdrBookName = Console.ReadLine();
                     Console.Write("\n  Enter Firstname to find and edit contact : ");
-                    findName = Console.ReadLine();
-                    CheckAddresssBook(searchAdrBookName);
 
-                    AddressBookMain.ModifyPersonInfo(searchAdrBookName, findName);
-                    AddressBookMain.DisplayContacts(searchAdrBookName);
-                    Operations();
                     break;
 
                 case 3:
+                    AddressBookMain.DisplayABList();
+
                     Console.Write("\n  Enter addressbook name to find and delete contact : ");
                     searchAdrBookName = Console.ReadLine();
                     Console.Write("\n  Enter Firstname to find and delete contact : ");
@@ -49,11 +51,9 @@ namespace NewAddressBook
                     break;
 
                 case 4:
+                    AddressBookMain.DisplayABList();
                     Console.Write("\n Here are available address books : ");
-                    foreach (var ab in AddressBookMain.contactsDictionary)
-                    {
-                        Console.Write("\t" + ab.Key);
-                    }
+
                     Console.Write("\n\n Enter address book name : ");
                     searchAdrBookName = Console.ReadLine();
                     AddressBookMain.DisplayContacts(searchAdrBookName);
@@ -78,6 +78,10 @@ namespace NewAddressBook
                     AddressBookMain.DisplayByCityState();
                     Operations();
                     break;
+                case 9:
+                    ContactFileOperation.FileOps();
+                    Operations();
+                    break;
 
                 case 0: break;
                 default:
@@ -86,16 +90,24 @@ namespace NewAddressBook
 
             void CheckAddresssBook(string searchAdrBookName)
             {
-                foreach (var ab in AddressBookMain.contactsDictionary)
-                {
-                    if ((ab.Key).ToUpper().Equals(searchAdrBookName.ToUpper()))
+                
+                
+                    int bookFound = 1;
+                    foreach (var ab in AddressBookMain.contactsDictionary)
                     {
-                        continue;
+                        if ((ab.Key).ToUpper().Equals(searchAdrBookName.ToUpper()))
+                            continue;
+                        else
+                            bookFound = 0;
                     }
+                    if (bookFound == 0)
+                    {
+                        Console.WriteLine(" --> Address book not found.");
+                        Operations();
+                    }
+
                 }
+
             }
-
         }
-
     }
-}
